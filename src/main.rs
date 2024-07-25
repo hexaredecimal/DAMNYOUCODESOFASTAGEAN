870e8e8b7a24698a788452b17df8d5c517e410d4
hexaredecimal <mfanakagama@gmail.com>
2024-07-25 21:34:40 +0200

    program: Remove window decorations and allow the window to be transparent

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-use async_std::{
-    channel::{self},
-    task,
-};
+use raylib::core::*;
 use raylib::prelude::*;
 use winit::{
-    event::{
-        ButtonId, DeviceEvent, ElementState, Event, KeyboardInput, MouseScrollDelta,
-        VirtualKeyCode, WindowEvent,
-    },
+    event::{DeviceEvent, ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::Window,
 };
-
-use clap::Parser; 
 
 pub(crate) enum HandledEvent {
     Keyboard(KeyboardInput),
@@ -24,26 +16,28 @@ pub(crate) enum HandledEvent {
     MouseScroll(MouseScrollDelta),*/
 }
 
-
-#[derive(Parser, Debug)]
-#[command(author, version, about)]
-struct Args {
-}
-
 fn main() {
-    let _ = Args::parse(); 
-
     let width = 200;
     let height = 200;
     let (mut rl, thread) = raylib::init()
         .size(width, height)
-        .title("DAMNYCODESOFASTAGEAN")
+        .title("TYPESOFASTJUTSU")
+        .undecorated()
+        .transparent()
         .build();
+
+    let screen_width = get_monitor_width(0);
+    let screen_height = get_monitor_height(0);
+
+    let padding = 10;
+    let window_x = screen_width - width - 10;
+    let window_y = screen_height * padding / 100;
+    rl.set_window_position(window_x, window_y);
 
     let mut images = vec![];
     for index in 1..=14 {
         let mut filename = String::from("images/frame");
-        let mut asserts_path = String::from("/usr/local/");
+        let mut asserts_path = String::from("./");
         filename.push_str(index.to_string().as_str());
         filename.push_str(".png");
         asserts_path.push_str(filename.as_str());
@@ -76,8 +70,8 @@ fn main() {
 
     // let origin = ffi::Vector2 {x: (frame_width * scale) as f32,  y: (frame_height * scale) as f32};
     let origin = ffi::Vector2 {
-        x: (frame_width * scale) as f32,
-        y: (frame_height * scale) as f32,
+        x: (frame_width * scale),
+        y: (frame_height * scale),
     };
 
     rl.set_target_fps(60);
@@ -89,7 +83,7 @@ fn main() {
     let window = Window::new(&event_loop).expect("Could not create x11 window");
     window.set_visible(false);
 
-    let mut ctrl_down = false; 
+    let mut ctrl_down = false;
 
     // let (tx, _) = channel::unbounded();
     while !rl.window_should_close() {
@@ -102,9 +96,9 @@ fn main() {
                     event: DeviceEvent::Key(key),
                     ..
                 } => {
-                    let current_key = key.clone();
+                    let current_key = key;
                     if current_key.state == ElementState::Pressed {
-                        return ();
+                        return;
                     }
 
                     let code = current_key.virtual_keycode.unwrap_or(VirtualKeyCode::Space);
@@ -112,107 +106,107 @@ fn main() {
                     match code {
                         VirtualKeyCode::A => {
                             index = 13;
-                            message.push_str("a");
+                            message.push('a');
                         }
                         VirtualKeyCode::B => {
                             index = 1;
-                            message.push_str("b");
+                            message.push('b');
                         }
                         VirtualKeyCode::C => {
                             index = 2;
-                            message.push_str("c");
+                            message.push('c');
                         }
                         VirtualKeyCode::D => {
                             index = 3;
-                            message.push_str("d");
+                            message.push('d');
                         }
                         VirtualKeyCode::E => {
                             index = 4;
-                            message.push_str("e");
+                            message.push('e');
                         }
                         VirtualKeyCode::F => {
                             index = 5;
-                            message.push_str("f");
+                            message.push('f');
                         }
                         VirtualKeyCode::G => {
                             index = 6;
-                            message.push_str("g");
+                            message.push('g');
                         }
                         VirtualKeyCode::H => {
                             index = 7;
-                            message.push_str("h");
+                            message.push('h');
                         }
                         VirtualKeyCode::I => {
                             index = 9;
-                            message.push_str("i");
+                            message.push('i');
                         }
                         VirtualKeyCode::J => {
                             index = 8;
-                            message.push_str("j");
+                            message.push('j');
                         }
                         VirtualKeyCode::K => {
                             index = 9;
-                            message.push_str("k");
+                            message.push('k');
                         }
                         VirtualKeyCode::L => {
                             index = 10;
-                            message.push_str("l");
+                            message.push('l');
                         }
                         VirtualKeyCode::M => {
                             index = 11;
-                            message.push_str("m");
+                            message.push('m');
                         }
                         VirtualKeyCode::N => {
                             index = 12;
-                            message.push_str("n");
+                            message.push('n');
                         }
                         VirtualKeyCode::O => {
                             index = 13;
-                            message.push_str("o");
+                            message.push('o');
                         }
                         VirtualKeyCode::P => {
                             index = 1;
-                            message.push_str("p");
+                            message.push('p');
                         }
                         VirtualKeyCode::Q => {
                             index = 2;
-                            message.push_str("q");
+                            message.push('q');
                         }
                         VirtualKeyCode::R => {
                             index = 3;
-                            message.push_str("r");
+                            message.push('r');
                         }
                         VirtualKeyCode::S => {
                             index = 4;
-                            message.push_str("s");
+                            message.push('s');
                         }
                         VirtualKeyCode::T => {
                             index = 5;
-                            message.push_str("t");
+                            message.push('t');
                         }
                         VirtualKeyCode::U => {
                             index = 6;
-                            message.push_str("u");
+                            message.push('u');
                         }
                         VirtualKeyCode::V => {
                             index = 7;
-                            message.push_str("v");
+                            message.push('v');
                         }
                         VirtualKeyCode::W => {
                             index = 8;
-                            message.push_str("w");
+                            message.push('w');
                         }
                         VirtualKeyCode::X => {
                             index = 9;
-                            message.push_str("x");
+                            message.push('x');
                         }
                         VirtualKeyCode::Y => {
                             index = 10;
-                            message.push_str("y");
+                            message.push('y');
                         }
                         VirtualKeyCode::Z => {
                             index = 11;
-                            message.push_str("z");
+                            message.push('z');
                         }
                         VirtualKeyCode::Return => {
                             index = 12;
@@ -244,15 +238,15 @@ fn main() {
                         }
                         VirtualKeyCode::Semicolon => {
                             index = 5;
-                            message.push_str(";");
+                            message.push(';');
                         }
                         VirtualKeyCode::Period => {
                             index = 6;
-                            message.push_str(".");
+                            message.push('.');
                         }
                         VirtualKeyCode::Apostrophe => {
                             index = 7;
-                            message.push_str("'");
+                            message.push('\'');
                         }
                         VirtualKeyCode::Tab => {
                             index = 8;
@@ -260,11 +254,11 @@ fn main() {
                         }
                         VirtualKeyCode::LBracket => {
                             index = 9;
-                            message.push_str("[");
+                            message.push('[');
                         }
                         VirtualKeyCode::RBracket => {
                             index = 10;
-                            message.push_str("]");
+                            message.push(']');
                         }
                         VirtualKeyCode::Back => {
                             index = 11;
@@ -272,66 +266,62 @@ fn main() {
                         }
                         VirtualKeyCode::Key1 => {
                             index = 12;
-                            message.push_str("1");
+                            message.push('1');
                         }
                         VirtualKeyCode::Key2 => {
                             index = 13;
-                            message.push_str("2");
+                            message.push('2');
                         }
                         VirtualKeyCode::Key3 => {
                             index = 0;
-                            message.push_str("3");
+                            message.push('3');
                         }
                         VirtualKeyCode::Key4 => {
                             index = 1;
-                            message.push_str("4");
+                            message.push('4');
                         }
                         VirtualKeyCode::Key5 => {
                             index = 2;
-                            message.push_str("5");
+                            message.push('5');
                         }
                         VirtualKeyCode::Key6 => {
                             index = 3;
-                            message.push_str("6");
+                            message.push('6');
                         }
                         VirtualKeyCode::Key7 => {
                             index = 4;
-                            message.push_str("7");
+                            message.push('7');
                         }
                         VirtualKeyCode::Key8 => {
                             index = 5;
-                            message.push_str("8");
+                            message.push('8');
                         }
                         VirtualKeyCode::Key9 => {
                             index = 6;
-                            message.push_str("9");
+                            message.push('9');
                         }
                         VirtualKeyCode::Key0 => {
                             index = 7;
-                            message.push_str("0");
+                            message.push('0');
                         }
                         VirtualKeyCode::Comma => {
                             index = 8;
-                            message.push_str(",");
+                            message.push(',');
                         }
                         VirtualKeyCode::LControl | VirtualKeyCode::RControl => {
                             index = 9;
-                            message.push_str("[CTRL]"); 
+                            message.push_str("[CTRL]");
 
-                            if ctrl_down {
-                                ctrl_down = false;
-                            } else {
-                                ctrl_down = true; 
-                            }
+                            ctrl_down = !ctrl_down;
 
-                            println!("CTRL key_down? {}", ctrl_down); 
+                            println!("CTRL key_down? {}", ctrl_down);
                         }
                         VirtualKeyCode::Delete => {
-                            index = 10; 
-                            message.push_str("[DEL]"); 
+                            index = 10;
+                            message.push_str("[DEL]");
                             if ctrl_down {
-                                *control_flow = ControlFlow::Exit; 
-                                std::process::exit(1); 
+                                *control_flow = ControlFlow::Exit;
+                                std::process::exit(1);
                             }
                         }
                         _ => {
